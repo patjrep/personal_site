10d729d9a068c39bc950140c7eff9eac241a0a95
Patrick Repaci <patrickrepaci@Patricks-MacBook-Pro-2.local>
2020-04-12 13:54:27 -0400

    removed files

--- toggle_text.js
+++
@@ -1,9 +0,0 @@
-
-function toggle_text() {
-    var text = document.querySelector(".email-box-reveal");
-    if (text.style.display === "none") {
-      text.style.display = "grid";
-    } else {
-      text.style.display = "none";
-    }
-  } 
