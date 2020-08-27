5e637de23f96371059c7443883e7fc3085036398
Patrick Repaci <patrickrepaci@hotmail.com>
2020-08-27 19:42:46 -0400

    added gif to typr and corresponding resizing code using js function

--- node.js
+++
@@ -1,7 +0,0 @@
-
-async function getGithubApi() {
-    let response = await fetch('https://api.github.com/repos/patjrep/typing_game');
-    let data = await response.json()
-    return data;
-}
-getGithubApi().then(data => console.log(data));
